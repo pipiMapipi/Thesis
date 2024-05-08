338d1951d6b39b35e060e8e706af61d23bd2720c
pipiMapipi <zhuoran.ma@outlook.com>
2024-05-07 23:16:09 -0400

    May7GrayBox

--- Thesis/Assets/Script/NavMesh/NavigationBaker.cs
+++ Thesis/Assets/Script/NavMesh/NavigationBaker.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.AI;
-using UnityEngine.AI;
 using NavMeshPlus.Components;
 
 public class NavigationBaker : MonoBehaviour
